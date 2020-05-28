dbb2e3f8b434bd744bccb76faee9e4f7e676424a
AnastasiiaKrush <anastasiia.krushelnytska@gmail.com>
2020-05-28 23:12:04 +0300

    Initialized react part

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {hot} from 'react-hot-loader';
+import './app.css';
+
+function App() {
+
+    return (
+        <div>
+            <h1>Head</h1>
+            <p>Paragraph</p>
+        </div>
+    );
+}
+
+export default hot(module)(App);
